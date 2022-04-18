09bc0d48c083c18be052596942f204536af81d0e
Jingzhou Wang <jw6460@nyu.edu>
2022-04-18 18:10:26 -0400

    change db url

--- config.py
+++ config.py
@@ -15,6 +15,9 @@ DATABASE_URI = os.getenv(
 #if 'VCAP_SERVICES' in os.environ:
 #    vcap = json.loads(os.environ['VCAP_SERVICES'])
 #    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+if 'DATABASE_URI' in os.environ:
+    db_url = json.loads(os.environ['DATABASE_URI'])
+    DATABASE_URI = db_url['DATABASE_URI']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
