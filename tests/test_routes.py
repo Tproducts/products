0eb5266e47424775606229978fbcacd971d0f234
226wyj <308033240@qq.com>
2022-04-19 18:01:06 +0000

    Fix create product test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,7 +139,7 @@ class TestProductServer(unittest.TestCase):
             new_product["description"], test_product.description, "Description do not match"
         )
         self.assertEqual(
-            new_product["price"], test_product.price, "Price does not match"
+            new_product["price"], int(test_product.price), "Price does not match"
         )
         # Check that the location header was correct
         resp = self.app.get(location, content_type=CONTENT_TYPE_JSON)
@@ -150,7 +150,7 @@ class TestProductServer(unittest.TestCase):
             new_product["description"], test_product.description, "Description do not match"
         )
         self.assertEqual(
-            new_product["price"], test_product.price, "Price does not match"
+            new_product["price"], int(test_product.price), "Price does not match"
         )
 
     def test_update_product(self):
