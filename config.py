589eaba234f80054c29b0ba67566b70b2f0e936b
Jingzhou Wang <jw6460@nyu.edu>
2022-04-26 22:40:43 -0400

    add url

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DATABASE_URI = os.getenv(
 # Override if running in Cloud Foundry
 if "VCAP_SERVICES" in os.environ:
     vcap = json.loads(os.environ["VCAP_SERVICES"])
-    DATABASE_URI = vcap["user-provided"][0]["credentials"]
+    DATABASE_URI = vcap["user-provided"][0]["credentials"]["url"]
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
